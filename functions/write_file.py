0a206a82df83307040a1c4128e71ddb65ad61745
pratikkshirsagar <meetpratiik@gmail.com>
2025-09-14 20:04:30 +0530

    write file function added

---
+++ functions/write_file.py
@@ -0,0 +1,28 @@
+import os
+
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    # Ensure the target path is within the working directory
+    try:
+        if os.path.commonpath([abs_working_dir, abs_file_path]) != abs_working_dir:
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    except Exception:
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+
+    parent_dir = os.path.dirname(abs_file_path)
+    if not os.path.isdir(parent_dir):
+        try:
+            os.makedirs(parent_dir)
+        except Exception as e:
+            return f"Error: Could not create parent directory '{parent_dir}': {e}"
+    try:
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
+    except Exception as e:
+        return f"Error: Failed to write to file '{file_path}': {e}"
--- tests.py
+++ tests.py
@@ -1,17 +1,14 @@
-from functions.get_files_info import get_files_info
+# from functions.get_files_info import get_files_info
+# from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 
 def main():
-    root_contents = get_files_info("calculator", ".")
-    print(root_contents)
-    pkg_content = get_files_info("calculator", "pkg")
-    print(pkg_content)
+    working_dir = "calculator"
 
-    error_content = get_files_info("calculator", "/bin")
-    print(error_content)
-
-    error_content = get_files_info("calculator", "../")
-    print(error_content)
+    print(write_file(working_dir, "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file(working_dir, "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file(working_dir, "/tmp/temp.txt", "this should not be allowed"))
 
 
 main()
